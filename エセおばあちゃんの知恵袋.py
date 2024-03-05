1c7fd0e28f5f2e95a91ed7059c61e2d89f0ff640
chroe <swan0616cell@gmail.com>
2024-03-05 15:42:05 +0900

    fix

---
+++ エセおばあちゃんの知恵袋.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import json
+import random
+import urllib
+
+with open('word_data.json', mode='r') as f:
+    word_json = json.load(f)
+
+noun_list = word_json['名詞']
+verb_list = word_json['動詞']
+adj_list = word_json['形容詞']
+
+st.title('エセおばあちゃんの知恵袋')
+st.write('エセおばあちゃんがエセ知恵袋を披露してくれます。')
+st.write('たまに文法がおかしいですが、おばあちゃんなので許してあげてください。')
+st.write('↓ボタンを押して生成')
+
+if st.button('教えて、おばあちゃん！'):
+    tips = f'{random.choice(noun_list)}を{random.choice(verb_list)}と{random.choice(adj_list)}んじゃよ'
+    with st.chat_message('おばあちゃん', avatar='👵'):
+        st.write(tips)
+    url = 'https://ba-chanapp.streamlit.app/'
+    text = f'{tips}\n\n#エセおばあちゃんの知恵袋\n{url}'
+    st.link_button('ツイート', f'https://twitter.com/intent/tweet?text={urllib.parse.quote(text)}')
+else:
+    with st.chat_message('おばあちゃん', avatar='👵'):
+        st.write('わしはなんでも知ってるぞい')
